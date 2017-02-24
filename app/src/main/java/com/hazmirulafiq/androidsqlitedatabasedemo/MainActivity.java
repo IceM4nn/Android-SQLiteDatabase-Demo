2589a9ce70bccedf539565e1a5c3005ccf4c913b
Hazmirul Afiq <hazmirulz@yahoo.com>
2017-02-24 16:56:09 +0800

    Updated

--- app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/MainActivity.java
+++ app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/MainActivity.java
@@ -1,16 +1,28 @@
 package com.hazmirulafiq.androidsqlitedatabasedemo;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseManager dbManager;
+    private ListView listView;
+    private SimpleCursorAdapter adapter;
+    private DatabaseHelper dbHelper;
+
+    final String[] from = new String[]{dbHelper._ID, dbHelper._TITLE, dbHelper._DESCRIPTION};
+    final int[] to = new int[]{R.id.id, R.id.listTitle, R.id.listDesc};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +31,57 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        dbManager = new DatabaseManager(this);
+        dbManager.open();
+        Cursor cursor = dbManager.fetch();
+
+        listView = (ListView) findViewById(R.id.myListView);
+
+        adapter = new SimpleCursorAdapter(this, R.layout.adapter, cursor, from, to, 0);
+        listView.setAdapter(adapter);
+
+        try {
+            Intent intent = getIntent();
+            Boolean ItemDeleted = intent.getExtras().getBoolean("ItemDeleted");
+            ModifyActivity modifyActivity = new ModifyActivity();
+            if (ItemDeleted) {
+                Snackbar.make(listView, "ItemDeleted!", Snackbar.LENGTH_LONG).show();
+                modifyActivity.setItemDeleted(false);
+            }
+        } catch (Exception e) {
+            if (adapter.isEmpty()) {
+                Snackbar.make(listView, "Click on fab to add list", Snackbar.LENGTH_LONG).show();
+            } else {
+                Snackbar.make(listView, "Hold on item to modify", Snackbar.LENGTH_LONG).show();
+            }
+        }
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,AddList.class);
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView itemID = (TextView) view.findViewById(R.id.id);
+                TextView itemTitle = (TextView) view.findViewById(R.id.listTitle);
+                TextView itemDesc = (TextView) view.findViewById(R.id.listDesc);
+
+                String myId = itemID.getText().toString();
+                String myTitle = itemTitle.getText().toString();
+                String myDesc = itemDesc.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), ModifyActivity.class);
+                intent.putExtra("Id", myId);
+                intent.putExtra("Title", myTitle);
+                intent.putExtra("Desc", myDesc);
                 startActivity(intent);
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+
+                return false;
             }
         });
+    }
+
+    public void onClickAddItem(View view) {
+        Intent intent = new Intent(getApplicationContext(), AddList.class);
+        startActivity(intent);
     }
 
     @Override
---
+++ app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/ModifyActivity.java
@@ -0,0 +1,80 @@
+package com.hazmirulafiq.androidsqlitedatabasedemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class ModifyActivity extends AppCompatActivity {
+
+    private EditText modTitle;
+    private EditText modDesc;
+    private long id;
+    private boolean isItemDeleted = false;
+
+    private DatabaseManager dbManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_modify);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        dbManager = new DatabaseManager(this);
+        dbManager.open();
+
+        modTitle = (EditText) findViewById(R.id.modTitle);
+        modDesc = (EditText) findViewById(R.id.modDesc);
+
+        Bundle intentData = getIntent().getExtras();
+
+        final String myID = intentData.getString("Id");
+        final String myTitle = intentData.getString("Title");
+        final String myDescription = intentData.getString("Desc");
+
+        modTitle.setText(myTitle);
+        modDesc.setText(myDescription);
+        id = Long.parseLong(myID);
+
+        FloatingActionButton fabDelete = (FloatingActionButton) findViewById(R.id.fabDelete);
+        FloatingActionButton fabUpdate = (FloatingActionButton) findViewById(R.id.fabUpdate);
+
+        fabDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbManager.delete(Integer.parseInt(myID));
+                setItemDeleted(true);
+                returnHome();
+            }
+        });
+
+        fabUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newTitle = modTitle.getText().toString();
+                String newDesc = modDesc.getText().toString();
+                dbManager.update(Integer.parseInt(myID),newTitle,newDesc);
+                returnHome();
+            }
+        });
+    }
+
+    public void returnHome() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if (isItemDeleted){
+            intent.putExtra("ItemDeleted", true);
+        }
+        startActivity(intent);
+    }
+
+    public void setItemDeleted(boolean itemDeleted) {
+        isItemDeleted = itemDeleted;
+    }
+}
