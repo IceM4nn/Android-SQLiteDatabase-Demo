7aad6c9259a6d3edc2f014aa13f338d3a7e7e461
Hazmirul Afiq <hazmirulz@yahoo.com>
2017-02-24 16:33:29 +0800

    Create new activity

--- app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/AddList.java
+++ app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/AddList.java
@@ -1,13 +1,19 @@
 package com.hazmirulafiq.androidsqlitedatabasedemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddList extends AppCompatActivity {
+
+    private EditText inputTitle;
+    private EditText inputDesc;
+    private DatabaseManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +21,30 @@ public class AddList extends AppCompatActivity {
         setContentView(R.layout.activity_add_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        inputTitle = (EditText) findViewById(R.id.titleInput);
+        inputDesc = (EditText) findViewById(R.id.descInput);
+
+        dbManager = new DatabaseManager(this);
+        dbManager.open();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String myInputTitle = inputTitle.getText().toString();
+                String myInputDesc = inputDesc.getText().toString();
+                if (myInputTitle.isEmpty() || myInputDesc.isEmpty()) {
+                    Snackbar.make(view,"Please fill in both form!",Snackbar.LENGTH_LONG).show();
+                } else {
+                    dbManager.insert(myInputTitle, myInputDesc);
+                    Intent intent = new Intent(AddList.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
--- app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/MainActivity.java
+++ app/src/main/java/com/hazmirulafiq/androidsqlitedatabasedemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hazmirulafiq.androidsqlitedatabasedemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,AddList.class);
+                startActivity(intent);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
